daa3962482585954e87d4b2a369c3ed4d8543724
emilian-d <71491036+emilian-d@users.noreply.github.com>
2020-12-07 09:33:15 +0200

    handle cases where password contains " at the start/end character

--- VismaBugBountySelfServicePortal/Helpers/CsvHelper.cs
+++ VismaBugBountySelfServicePortal/Helpers/CsvHelper.cs
@@ -10,12 +10,18 @@ namespace VismaBugBountySelfServicePortal.Helpers
 
         public static IEnumerable<string> GetCsvElements(this string line, string separator = ",")
         {
-            if(separator != "," && separator != ";")
-                throw new ArgumentOutOfRangeException(nameof(separator),"separator should be: , or ;");
+            if (separator != "," && separator != ";")
+                throw new ArgumentOutOfRangeException(nameof(separator), "separator should be: , or ;");
             var regex = new Regex(string.Format(CsvRegex, separator), RegexOptions.None, TimeSpan.FromSeconds(1));
             foreach (Match match in regex.Matches(line))
             {
-                var value = match.Value.Trim(' ', '"', ',');
+                var value = match.Value;
+
+                if (value.StartsWith(separator))
+                    value = value[1..];
+                if (value.StartsWith("\"") && value.EndsWith("\""))
+                    value = value[1..^1];
+                value = value.Replace("\"\"", "\"");
                 yield return value;
             }
         }
